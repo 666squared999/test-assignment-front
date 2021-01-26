af8781bdd9e1135033af619b4db90430eac499fe
Anna Kryva <anna120100@gmail.com>
2021-01-26 03:58:21 +0200

    fix: delete unnecessary button component

--- src/components/Button/index.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC } from "react";
-import { observer } from "mobx-react";
-import "./style.scss";
-
-export const Button: FC = observer(() => {
-    return <button className="Button">Hiiii</button>;
-});
