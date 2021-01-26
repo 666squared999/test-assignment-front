75cc159a7d0cb53d53c3a4901e24b22e1c2a6631
Anna Kryva <anna120100@gmail.com>
2021-01-26 03:41:11 +0200

    feat: add table pagination and loading indicator

--- src/components/TableWrapper/index.tsx
+++ src/components/TableWrapper/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useCallback, useEffect, useState } from "react";
 import { observer } from "mobx-react";
 import "./style.scss";
 import {
@@ -10,8 +10,12 @@ import {
     TableRow,
     Paper,
     TableSortLabel,
+    TableFooter,
+    TablePagination,
+    CircularProgress,
 } from "@material-ui/core";
 import { getBuckWeat } from "../../api/requests";
+import TablePaginationActions from "../../utils/TablePaginationActions";
 
 interface IDataUnit {
     id: string;
@@ -32,12 +36,28 @@ enum ShopNames {
 
 export const TableWrapper: FC = observer(() => {
     const [rows, setRows] = useState<IDataUnit[]>([]);
-    useEffect(() => {
-        handleFetchData();
-    }, []);
+    const [loading, setLoading] = useState<boolean>();
 
-    const handleFetchData = async () => {
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+
+    const handleChangePage = (
+        event: React.MouseEvent<HTMLButtonElement> | null,
+        newPage: number,
+    ) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (
+        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    ) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const handleFetchData = useCallback(async () => {
         try {
+            setLoading(true);
             const response = await getBuckWeat();
             const data = await response.json();
             console.log(data);
@@ -57,34 +77,82 @@ export const TableWrapper: FC = observer(() => {
             console.log(preparedData);
 
             setRows(preparedData);
+            setLoading(false);
         } catch (e) {
             console.log("Error fetching data", e);
         }
-    };
+    }, []);
+
+    useEffect(() => {
+        handleFetchData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
-        <TableContainer component={Paper}>
-            <Table size="medium">
-                <TableHead>
-                    <TableRow>
-                        <TableCell sortDirection="desc">
-                            Найменування товару
-                        </TableCell>
-                        <TableCell>Ціна за 1 кг, грн</TableCell>
-                        <TableCell>Вага, кг</TableCell>
-                        <TableCell>Магазин</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row: IDataUnit) => (
-                        <TableRow key={row.title}>
-                            <TableCell component="th">{row.title}</TableCell>
-                            <TableCell>{row.price_per_kg}</TableCell>
-                            <TableCell>{row.weight}</TableCell>
-                            <TableCell>{row.shopName}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <div className="Table">
+            {loading ? (
+                <CircularProgress color="secondary" />
+            ) : (
+                <TableContainer component={Paper}>
+                    <Table size="medium">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell sortDirection="desc">
+                                    Найменування товару
+                                </TableCell>
+                                <TableCell>Ціна за 1 кг, грн</TableCell>
+                                <TableCell>Вага, кг</TableCell>
+                                <TableCell>Магазин</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {(rowsPerPage > 0
+                                ? rows.slice(
+                                      page * rowsPerPage,
+                                      page * rowsPerPage + rowsPerPage,
+                                  )
+                                : rows
+                            ).map((row: IDataUnit) => (
+                                <TableRow key={row.title}>
+                                    <TableCell component="th">
+                                        {row.title}
+                                    </TableCell>
+                                    <TableCell>{row.price_per_kg}</TableCell>
+                                    <TableCell>{row.weight}</TableCell>
+                                    <TableCell>{row.shopName}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                        <TableFooter>
+                            <TableRow>
+                                <TablePagination
+                                    rowsPerPageOptions={[
+                                        10,
+                                        25,
+                                        50,
+                                        { label: "All", value: -1 },
+                                    ]}
+                                    colSpan={3}
+                                    count={rows.length}
+                                    rowsPerPage={rowsPerPage}
+                                    page={page}
+                                    SelectProps={{
+                                        inputProps: {
+                                            "aria-label": "rows per page",
+                                        },
+                                        native: true,
+                                    }}
+                                    onChangePage={handleChangePage}
+                                    onChangeRowsPerPage={
+                                        handleChangeRowsPerPage
+                                    }
+                                    ActionsComponent={TablePaginationActions}
+                                />
+                            </TableRow>
+                        </TableFooter>
+                    </Table>
+                </TableContainer>
+            )}
+        </div>
     );
 });
